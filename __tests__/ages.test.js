4b401139a5206258299fc39d648d956a1d9a52bd
Andrew <abatesaccs@gmail.com>
2019-11-15 13:01:27 -0800

    test life expectancy for mercury

--- __tests__/ages.test.js
+++ __tests__/ages.test.js
@@ -6,22 +6,22 @@ describe ('Ages', () => {
       });
     test('should check that mercury returns the correct age', () => {
         var myAge = new Age(1);
-        expect(myAge.toMercury(1)).toEqual(.24);
+        expect(myAge.toMercury(1)).toEqual(1/.24);
     });
     test('should check that venus returns the correct age', () => {
         var myAge = new Age(1);
-        expect(myAge.toVenus(1)).toEqual(.62);
+        expect(myAge.toVenus(1)).toEqual(1/.62);
     });
     test('should check that mars returns the correct age', () => {
         var myAge = new Age(1);
-        expect(myAge.toMars(1)).toEqual(1.88);
+        expect(myAge.toMars(1)).toEqual(1/1.88);
     });
     test('should check that jupiter returns the correct age', () => {
         var myAge = new Age(1);
-        expect(myAge.toJupiter(1)).toEqual(11.86);
+        expect(myAge.toJupiter(1)).toEqual(1/11.86);
     });
     test('should check that life expectancy is correct', () => {
         var myAge = new Age(1);
-        expect(myAge.lifeExpectancy(1)).toEqual(.24);
+        expect(myAge.lifeExpectancy(myAge.toMercury(),"mercury")).toEqual(((71 / .24) - myAge.toMercury()));
     });
 });
--- src/ages.js
+++ src/ages.js
@@ -8,32 +8,32 @@ export class Age{
         this.averageLife = 71;
     }
     toMercury(){
-        let temp = (this.age.toFixed(2) * this.mercury);
+        let temp = (this.age.toFixed(2) / this.mercury);
         return temp;
     }
     toVenus(){
-        let temp = (this.age.toFixed(2) * this.venus);
+        let temp = (this.age.toFixed(2) / this.venus);
         return temp;
     }
     toMars(){
-        let temp = (this.age.toFixed(2) * this.mars);
+        let temp = (this.age.toFixed(2) / this.mars);
         return temp;
     }  
     toJupiter(){
-        let temp = (this.age.toFixed(2) * this.jupiter);
+        let temp = (this.age.toFixed(2) / this.jupiter);
         return temp;
     }
     lifeExpectancy(age, planet){
-        let averageAge = 70;
+        let averageAge = 71;
         let choice = planet.toLowerCase();
         if(choice == "mercury"){
-            
+            return ((averageAge / this.mercury) - age);
         } else if (choice == "venus"){
-
+            return ((averageAge / this.venus) - age);
         } else if (choice == "mars"){
-
+            return ((averageAge / this.mars) - age);
         } else if (choice == "jupiter"){
-
+            return ((averageAge / this.jupiter) - age);
         }
     }
 };
