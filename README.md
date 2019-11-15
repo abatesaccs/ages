80ae3356afb7c24dddfce33cbb6f8e23c645c0df
Andrew <abatesaccs@gmail.com>
2019-11-15 15:19:50 -0800

    move readme into proper folder

---
+++ README.md
@@ -0,0 +1,95 @@
+# _Super Galactic Age Calculator_
+
+#### _An age calculator for the first five planets. 11/1/2019_
+
+#### By _**Andrew Bates**_
+
+## Description
+
+_This is a program that reads user input to create a pizza using chosen toppings, and decides a price depending on parameters chosen._
+
+## Specifications
+
+_Spec: return a user's age in mercury years._
+  * _Input: 5_
+  * _Output: 20.83_
+
+_Spec: return a user's age in venus years._
+  * _Input: 5_
+  * _Output: 8.06_
+
+_Spec: return a user's age in mars years._
+  * _Input: 5_
+  * _Output: 2.66_
+
+_Spec: return a user's age in jupiter years._
+  * _Input: 5_
+  * _Output: .42_
+
+_Spec: return life expectancy minus years lived in mercury years._
+  * _Input: 5_
+  * _Output: 50.17_
+
+_Spec: return life expectancy minus years lived in venus years._
+* _Input: 5_
+* _Output: 106.45_
+
+_Spec: return life expectancy minus years lived in mars years._
+  * _Input: 5_
+  * _Output: 35.11_
+
+_Spec: return life expectancy minus years lived in jupiter years._
+  * _Input: 5_
+  * _Output: 5.56_
+
+## Setup/Installation Requirements
+
+* _note: there is currently no UI._
+
+* _Go to https://github.com/abatesaccs/ages_
+* _Clone or download all files in the repository (make sure they are contained in the same file!)_
+* _install node.js and npm_
+* _run npm install in the terminal_
+* _run npm start in the terminal_
+
+* _OR go to: https://abatesaccs.github.io/ages/_
+
+_The file may have inconsistencies when using older browser versions_
+
+## Known Bugs
+
+_There is currently no UI!_
+
+## Support and contact details
+
+_If you run into any issues, or have any trouble, contact me:
+ Andrew Bates
+ email: abatesaccs@gmail.com_
+
+## Technologies Used
+
+_Atom, Bootstrap, jQuery, node.js, webpack, jest_
+
+### License
+
+*Copyright (c) 2019 Andrew Bates
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*
+
+Copyright (c) 2016 **_Andrew Bates_**
--- __tests__/ages.test.js
+++ __tests__/ages.test.js
@@ -1,61 +1,60 @@
 import {Age} from './../src/ages.js';
 
 describe ('Ages', () => {
+    let myAge;
+    let myOldAge;
     beforeEach(() => {
-        var myAge = new Age(1);
+        myAge = new Age(1);
+        myOldAge = new Age(75);
       });
     test('should check that mercury returns the correct age', () => {
-        let myAge = new Age(1);
         expect(myAge.toMercury(1)).toEqual(1/.24);
     });
     test('should check that venus returns the correct age', () => {
-        let myAge = new Age(1);
         expect(myAge.toVenus(1)).toEqual(1/.62);
     });
     test('should check that mars returns the correct age', () => {
-        let myAge = new Age(1);
         expect(myAge.toMars(1)).toEqual(1/1.88);
     });
     test('should check that jupiter returns the correct age', () => {
-        let myAge = new Age(1);
         expect(myAge.toJupiter(1)).toEqual(1/11.86);
     });
 });
 
 describe ('life expectancy', () => {
+    let myAge;
+    beforeEach(() => {
+        myAge = new Age(1);
+      });
     test('should check that mercurys life expectancy is correct', () => {
-        let myAge = new Age(1);
         expect(myAge.lifeExpectancy("mercury")).toEqual(((71 / .24) - (1/.24)));
     });
     test('should check that venus life expectancy is correct', () => {
-        let myAge = new Age(1);
         expect(myAge.lifeExpectancy("venus")).toEqual(((71 / .62) - (1/.62)));
     });
     test('should check that mars life expectancy is correct', () => {
-        let myAge = new Age(1);
         expect(myAge.lifeExpectancy("mars")).toEqual(((71 / 1.88) - (1/1.88)));
     });
     test('should check that jupiters life expectancy is correct', () => {
-        let myAge = new Age(1);
         expect(myAge.lifeExpectancy("jupiter")).toEqual(((71 / 11.86) - (1/11.86)));
     });
 });
 
 describe ('exceeded life expectancy', () => {
+    let myOldAge;
+    beforeEach(() => {
+        myOldAge = new Age(75);
+      });
     test('should return years lived past life expectancy', () => {
-        let myAge = new Age(72);
-        expect(myAge.lifeExpectancy("mercury")).toBeCloseTo(((72 / .24) - (71/.24)), 8);
+        expect(myOldAge.lifeExpectancy("mercury")).toBeCloseTo(((75 / .24) - (71/.24)), 8);
     });
     test('should return years lived past life expectancy', () => {
-        let myAge = new Age(72);
-        expect(myAge.lifeExpectancy("venus")).toBeCloseTo(((72 / .62) - (71/.62)), 8);
+        expect(myOldAge.lifeExpectancy("venus")).toBeCloseTo(((75 / .62) - (71/.62)), 8);
     });
     test('should return years lived past life expectancy', () => {
-        let myAge = new Age(72);
-        expect(myAge.lifeExpectancy("mars")).toBeCloseTo(((72 / 1.88) - (71/1.88)), 8);
+        expect(myOldAge.lifeExpectancy("mars")).toBeCloseTo(((75 / 1.88) - (71/1.88)), 8);
     });
     test('should return years lived past life expectancy', () => {
-        let myAge = new Age(72);
-        expect(myAge.lifeExpectancy("jupiter")).toBeCloseTo(((72 / 11.86) - (71/11.86)), 8);
+        expect(myOldAge.lifeExpectancy("jupiter")).toBeCloseTo(((75 / 11.86) - (71/11.86)), 8);
     });
 });
